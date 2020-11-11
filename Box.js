59a1eace83357350af1d5ae3301c927b5f5c30b2
pushpalathach2505 <73642079+pushpalathach2505@users.noreply.github.com>
2020-11-11 15:24:55 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        var box_options={
+            'restitution':1.5,
+           
+        }
+        this.body=Bodies.rectangle(x,y,width,height,box_options);
+        World.add(world,this.body);
+        this.width=width;
+        this.height=height;
+    }
+
+    display()
+    {
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rect(0,0,this.width,this.height);
+    pop();
+}
+}
---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+   box1=new Box(200,200,50,50);
+   box2=new Box(230,100,50,150);
+    console.log(ground);
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+box1.display();
+console.log(box1.body.angle);
+console.log(box2.body.angle);
+box2.display();
+}
